010a912b9b54384a01704b7101ded3a643bac03f
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-05-20 10:43:31 -0300

    atv1

---
+++ Aula2/Ex1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+Escreva uma função que retorne o valor de n!.
+*/
+
+int fatorial(int n){
+
+    int resultado, i;
+    resultado = 1;
+    for(i=n;i>=1;i--){
+        resultado=resultado*i;
+    }
+    return resultado;
+}
+
+int main(){
+
+    int numero,fat;
+
+    printf("Informe o numero:\n");
+    scanf("%d",numero);
+    fat=fatorial(numero);
+    printf("O fatorial de %d eh %d\n",numero,fat);
+
+    return 0;
+}

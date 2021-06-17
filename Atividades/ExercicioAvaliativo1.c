06d33fdf6195a16a2144e99738d0da60a4e6caf1
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-06-17 15:41:17 -0300

    Update aval

--- Atividades/ExercicioAvaliativo1.c
+++ Atividades/ExercicioAvaliativo1.c
@@ -2,27 +2,44 @@
 #include <stdlib.h>
 #define dim 3
 
-void printBoard(char board[][dim]){
+void printBoard(int board[][dim]){
     int i,j;
-    putchar("\n");
+    putchar('\n');
     for(i=0;i<dim;i++){
         for(j=0;j<dim;j++){
-            if(board[i][j] == 0)
+            if(board[i][j] == 0){
+                printf("   ");
+            }else if(board[i][j] == 1){
                 printf(" X ");
-            else
+            }else{
                 printf(" O ");
-            if(j != (dim-1))
-                printf("|");         
+            }
+            if(j != (dim-1)){
+                printf("|");
+            }         
         }
-        putchar("\n");
+        putchar('\n');
     }
-    putchar("\n");
+    putchar('\n');
+}
+
+int checkLocal(int board[][dim], int x, int y){
+    if(x<0 || x>(dim-1) || y<0 || y>(dim-1) || board[x][y]!=0){
+
+    }
 }
 
 int main(){
-    char board[dim][dim];/*={'x','o','x',
-                      'x','x','o',
-                      'x','o','o'};*/
+    int board[dim][dim],i,j;
+
+    /*for(i=0;i<dim;i++){
+        for(j=0;j<dim;j++){
+            printf("Informe o valor na posicao [%d][%d]: ",i,j);
+            scanf("%d", &board[i][j]);
+            printBoard(board);
+        }
+    }*/
+
     printBoard(board);
     
 

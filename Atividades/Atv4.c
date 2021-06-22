a29b217b28f59b7b4bf5282489a2661d6b9c1fc5
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-06-22 10:40:13 -0300

    update atv4

--- Atividades/Atv4.c
+++ Atividades/Atv4.c
@@ -5,8 +5,28 @@
 Escreva uma função em C que, dada uma matriz, retorne o valor do seu determinante.
 */
 
+int determinante(int **mat,int m, int n){
+    
+}
+
 int main(){
-
-
+    int m,n,i,j;
+    printf("Informe o tamanho da matriz: ");
+    scanf("%d %d", &m, &n);
+    int mat[m][n];
+    printf("Informe os valores na matriz:\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("Mat[%d][%d]: ",i,j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    /*
+    for(i=0;i<m;i++){
+        for(j=0;j<2;j++){
+            printf("%d\n", mat[i][j]);
+        }
+    }
+    */
     return 0;
 }

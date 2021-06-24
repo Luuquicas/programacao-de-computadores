81d4021f8253f00b821b7d7ad38de1fecec3dd96
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-06-24 10:24:23 -0300

    create atv5

---
+++ Atividades/Atv5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Criei um programa, em C, para implementar um sorteio. Nesse jogo, o usuário deverá selecionar a quantidade de números que ele deseja apostar (entre 1 e 20), em seguida, informar os valores escolhidos (entre 0 e 100). Após receber tais valores o programa deverá sortear, aleatoriamente, 20 valores (entre 0 e 100). Após o sorteio, o programa deverá comparar os valores apostados com os sorteados e retornar para o usuário a quantidade de acertos e os números que ele acertou.
+O programa deverá implementar as seguintes funções:
+
+void ler_aposta(int *aposta,int n);
+void sorteia_valores(int *sorteio,int n);
+int compara_aposta(int *aposta, int *sorteio,int *val_certos,int na, int ns);
+
+É obrigatório o uso de alocação dinâmica de memoria.
+*/
+
+int main(){
+
+    return 0;
+}

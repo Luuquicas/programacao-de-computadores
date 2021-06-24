516c2a56f8c9dc7a49f9704f521110200f8fd52a
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-06-24 10:25:42 -0300

    update

--- Atividades/Atv5.c
+++ Atividades/Atv5.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /*
-Criei um programa, em C, para implementar um sorteio. Nesse jogo, o usuário deverá selecionar a quantidade de números que ele deseja apostar (entre 1 e 20), em seguida, informar os valores escolhidos (entre 0 e 100). Após receber tais valores o programa deverá sortear, aleatoriamente, 20 valores (entre 0 e 100). Após o sorteio, o programa deverá comparar os valores apostados com os sorteados e retornar para o usuário a quantidade de acertos e os números que ele acertou.
+Crie um programa, em C, para implementar um sorteio. Nesse jogo, o usuário deverá selecionar a quantidade de números que ele deseja apostar (entre 1 e 20), em seguida, informar os valores escolhidos (entre 0 e 100). Após receber tais valores o programa deverá sortear, aleatoriamente, 20 valores (entre 0 e 100). Após o sorteio, o programa deverá comparar os valores apostados com os sorteados e retornar para o usuário a quantidade de acertos e os números que ele acertou.
 O programa deverá implementar as seguintes funções:
 
 void ler_aposta(int *aposta,int n);

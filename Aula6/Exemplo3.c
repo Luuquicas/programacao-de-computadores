864fa879a93bb5f7c22c812308c5178b9d422f4d
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-07-01 11:30:27 -0300

    update exemplo3

--- Aula6/Exemplo3.c
+++ Aula6/Exemplo3.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
+    int i, j, linha, coluna, **m;
+    printf("Informe a quantidade de linhas e colunas da matriz: ");
+    scanf("%d %d", &linha, &coluna);
 
+    //Alocar dinamicamente a primeira dimensao
+    m = (int **)malloc(linha * sizeof(int *));
+
+    //Alocar dinamicamente a segunda dimensao
+    for (i = 0; i < linha; i++){
+        m[i] = (int *)malloc(coluna * sizeof(int));
+    }
+
+    printf("informe os valores da matriz: ");
+    for(i=0;i<linha;i++){
+        for(j=0;j<coluna;j++){
+            scanf("%d", m[i][j]);
+        }
+    }
+
+    printf("Matriz: \n");
+    for(i=0;i<linha;i++){
+        for(j=0;j<coluna;j++){
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+    //Liberar memoria
+    for(i=0;i<linha;i++){
+        free(m[i]);
+    }
+    free(m);
 
     return 0;
 }
